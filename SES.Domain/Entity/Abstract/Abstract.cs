4e8cb0eb3b5f511c7ee461e0ff22521af39f1a2a
ApachUA <4971779@gmail.com>
2023-09-20 12:59:30 +0300

    Created Abstract class

---
+++ SES.Domain/Entity/Abstract/Abstract.cs
@@ -0,0 +1,20 @@
+﻿using SES.Domain.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SES.Domain.Entity.Abstract
+{
+	public class Abstract
+	{
+		public int Abstract_ID { get; set; }
+
+		public required string Name { get; set; }
+
+		public required string URL { get; set; }
+
+		public required Area_of_study AreaOfStudy { get; set; }
+	}
+}
