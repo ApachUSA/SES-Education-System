93cfd33f8cc435db64ec486f14db2cef7892ea50
ApachUA <4971779@gmail.com>
2023-09-20 13:28:54 +0300

    Created user entity

--- SES.Domain/Entity/User/User.cs
+++ SES.Domain/Entity/User/User.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SES.Domain.Enum;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,30 @@ using System.Threading.Tasks;
 
 namespace SES.Domain.Entity.User
 {
-	internal class User
+	public class User
 	{
+		public int User_ID { get; set; }
+
+		public required string Surname { get; set;}
+
+		public required string Name { get; set;}
+
+		public required string Patronymic { get; set;}
+
+		public required int Position_ID { get; set; }
+
+		public Position? Position { get; set; }
+
+		public required int Rang_ID { get; set; }
+
+		public Rang? Rang { get; set; }
+
+		public required string Login { get; set; }
+
+		public required string Password { get; set; }
+
+		public required Role Role_ID { get; set; }
+
+
 	}
 }
