1a50da3ade573fae3098089a313acc69a431f458
ApachUA <4971779@gmail.com>
2023-09-20 12:54:47 +0300

    Created enums (area, region, role)

---
+++ SES.Domain/Enum/Area_of_study.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SES.Domain.Enum
+{
+	public enum Area_of_study
+	{
+		Функціональна,
+		Психологічна,
+		Домедична,
+		Профільна,
+		Гуманітарна,
+		Додаткова
+	}
+
+}
