2c49acd0f36da71e37cdc413e289dac0e1da028b
ApachUA <4971779@gmail.com>
2023-09-21 12:41:27 +0300

    Use dbcontext for test

--- SES.Web/Controllers/HomeController.cs
+++ SES.Web/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SES.Infrastructure;
 using SES.Web.Models;
 using System.Diagnostics;
 
@@ -7,14 +8,17 @@ namespace SES.Web.Controllers
 	public class HomeController : Controller
 	{
 		private readonly ILogger<HomeController> _logger;
+		private readonly SES_DB_Context context;
 
-		public HomeController(ILogger<HomeController> logger)
+		public HomeController(ILogger<HomeController> logger, SES_DB_Context context)
 		{
 			_logger = logger;
+			this.context = context;
 		}
 
 		public IActionResult Index()
 		{
+			
 			return View();
 		}
 
