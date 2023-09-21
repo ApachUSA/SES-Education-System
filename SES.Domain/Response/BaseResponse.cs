e7a3aca814b48efa0eea15657db9ca6774bfaca7
ApachUA <4971779@gmail.com>
2023-09-21 13:24:55 +0300

    Created response class

---
+++ SES.Domain/Response/BaseResponse.cs
@@ -0,0 +1,28 @@
+﻿using SES.Domain.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SES.Domain.Response
+{
+	public class BaseResponse<T>
+	{
+		public string Description { get; set; }
+		public ResponseStatus StatusCode { get; set; }
+		public T Data { get; set; }
+
+		public static BaseResponse<T> Success(T data)
+		{
+			return new BaseResponse<T> { StatusCode = ResponseStatus.Success, Data = data };
+		}
+
+		public static BaseResponse<T> Fail(ResponseStatus responseStatus, string description)
+		{
+			return new BaseResponse<T> { StatusCode = responseStatus, Description = description };
+		}
+	}
+
+	
+}
