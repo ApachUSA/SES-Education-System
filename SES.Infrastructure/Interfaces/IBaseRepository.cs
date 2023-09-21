2acdbd9ec025f620751ce88cee333bde0f382019
ApachUA <4971779@gmail.com>
2023-09-21 13:02:47 +0300

    Developed IBaseRepository and implemented them

---
+++ SES.Infrastructure/Interfaces/IBaseRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SES.Infrastructure.Interfaces
+{
+	public interface IBaseRepository<T> where T : class
+	{
+		Task Create(T entity);
+
+		Task Delete(T entity);
+
+		IQueryable<T> Get();
+
+		Task<T> Update(T entity);
+
+		Task<List<T>> Update(List<T> entity);
+	}
+}
---
+++ SES.Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,52 @@
+﻿using SES.Infrastructure.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SES.Infrastructure.Repositories
+{
+	public class Repository<T> : IBaseRepository<T> where T : class
+	{
+		private readonly SES_DB_Context _dbContext;
+
+		public Repository(SES_DB_Context dbContext)
+		{
+			_dbContext = dbContext;
+		}
+
+		public async Task Create(T entity)
+		{
+			await _dbContext.Set<T>().AddAsync(entity);
+			await _dbContext.SaveChangesAsync();
+		}
+
+		public async Task Delete(T entity)
+		{
+			_dbContext.Set<T>().Remove(entity);
+			await _dbContext.SaveChangesAsync();
+		}
+
+		public IQueryable<T> Get()
+		{
+			return _dbContext.Set<T>();
+		}
+
+		public async Task<T> Update(T entity)
+		{
+			_dbContext.Set<T>().Update(entity);
+			await _dbContext.SaveChangesAsync();
+
+			return entity;
+		}
+
+		public async Task<List<T>> Update(List<T> entity)
+		{
+			_dbContext.Set<T>().UpdateRange(entity);
+			await _dbContext.SaveChangesAsync();
+
+			return entity;
+		}
+	}
+}
