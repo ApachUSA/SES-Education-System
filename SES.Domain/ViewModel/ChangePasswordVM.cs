35c102424198f902e5c5bf0ccfe806ee5107fc38
ApachUA <4971779@gmail.com>
2023-09-21 17:17:42 +0300

    Added some VM

---
+++ SES.Domain/ViewModel/ChangePasswordVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SES.Domain.ViewModel
+{
+	public class ChangePasswordVM
+	{
+		public required string Password { get; set; }
+
+		public required string ConfirmPassword { get; set; }
+	}
+}
---
+++ SES.Domain/ViewModel/RegisterVM.cs
@@ -0,0 +1,33 @@
+﻿using SES.Domain.Entity.UserE;
+using SES.Domain.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SES.Domain.ViewModel
+{
+	public class RegisterVM
+	{
+		public required string Snp { get; set; }
+
+		public required int Department_ID { get; set; }
+
+		public Department? Department { get; set; }
+
+		public required int Position_ID { get; set; }
+
+		public Position? Position { get; set; }
+
+		public required int Rang_ID { get; set; }
+
+		public Rang? Rang { get; set; }
+
+		public required string Login { get; set; }
+
+		public required string Password { get; set; }
+
+		public required Role Role_ID { get; set; }
+	}
+}
