a51e9a3e309cd087caa74a87dceb773f9c5169e9
ApachUA <4971779@gmail.com>
2023-09-21 12:39:37 +0300

    Fixed DbContext

--- SES.Infrastructure/SES_DB_Context.cs
+++ SES.Infrastructure/SES_DB_Context.cs
@@ -23,10 +23,10 @@ namespace SES.Infrastructure
 		public DbSet<Question> Questions { get; set; }
 		public DbSet<Right_Answer> RightAnswers { get; set; }
 		public DbSet<Test> Tests { get; set; }
-		public DbSet<Test_History> Tests_History { get; set; }
+		public DbSet<Test_History> Tests_Histories { get; set; }
 		public DbSet<Test_Result> Test_Results { get; set; }
 
-		public DbSet<City> Citys { get; set; }
+		public DbSet<City> Cities { get; set; }
 		public DbSet<Department> Departments { get; set; }
 		public DbSet<Position> Positions { get; set; }
 		public DbSet<Rang> Rangs { get; set; }
@@ -36,14 +36,14 @@ namespace SES.Infrastructure
 		{
 			modelBuilder.Entity<Abstract>(builder =>
 			{
-				builder.ToTable("Abstract").HasKey(x => x.Abstract_ID);
+				builder.ToTable("Abstracts").HasKey(x => x.Abstract_ID);
 				builder.Property(x => x.Abstract_ID).ValueGeneratedOnAdd();
 			});
 
 
 			modelBuilder.Entity<Option>(builder =>
 			{
-				builder.ToTable("Option").HasKey(x => x.Option_ID);
+				builder.ToTable("Options").HasKey(x => x.Option_ID);
 				builder.Property(x => x.Option_ID).ValueGeneratedOnAdd();
 
 				builder.HasOne(x => x.Question)
@@ -51,90 +51,97 @@ namespace SES.Infrastructure
 				.HasForeignKey(x => x.Question_ID)
 				.OnDelete(DeleteBehavior.Cascade);
 
-				builder.HasOne(x => x.Right_Answer)
-				.WithOne(x => x.Option)
-				.HasForeignKey<Right_Answer>(x => x.Option_ID);
 
 			});
 
 			modelBuilder.Entity<Question>(builder =>
 			{
-				builder.ToTable("Question").HasKey(x => x.Question_ID);
+				builder.ToTable("Questions").HasKey(x => x.Question_ID);
 				builder.Property(x => x.Question_ID).ValueGeneratedOnAdd();
 
 				builder.HasOne(x => x.Test)
 				.WithMany(x => x.Questions)
-				.HasForeignKey(x => x.Test_ID);
+				.HasForeignKey(x => x.Test_ID)
+				.OnDelete(DeleteBehavior.Cascade);
 			});
 
 			modelBuilder.Entity<Right_Answer>(builder =>
 			{
-				builder.ToTable("Right_Answer").HasKey(x => x.Right_Answer_ID);
+				builder.ToTable("RightAnswers").HasKey(x => x.Right_Answer_ID);
 				builder.Property(x => x.Right_Answer_ID).ValueGeneratedOnAdd();
 
 				builder.HasOne(x => x.Option)
-					.WithOne(x => x.Right_Answer)
-					.HasForeignKey<Option>(x => x.Option_ID);
+					.WithMany(x => x.Right_Answers)
+					.HasForeignKey(x => x.Option_ID)
+					.OnDelete(DeleteBehavior.Cascade);
 
 				builder.HasOne(x => x.Question)
 				.WithMany(x => x.Right_Answers)
-				.HasForeignKey(x => x.Question_ID);
+				.HasForeignKey(x => x.Question_ID)
+				.OnDelete(DeleteBehavior.NoAction);
 
 			});
 
 
 			modelBuilder.Entity<Test>(builder =>
 			{
-				builder.ToTable("Test").HasKey(x => x.Test_ID);
+				builder.ToTable("Tests").HasKey(x => x.Test_ID);
 				builder.Property(x => x.Test_ID).ValueGeneratedOnAdd();
 
 				builder.HasOne(x => x.Position)
 					.WithMany(x => x.Tests)
-					.HasForeignKey(x => x.Position_ID);
+					.HasForeignKey(x => x.Position_ID)
+				.OnDelete(DeleteBehavior.NoAction);
+
 
 			});
 
 			modelBuilder.Entity<Test_History>(builder =>
 			{
-				builder.ToTable("Test_History").HasKey(x => x.Test_History_ID);
+				builder.ToTable("Tests_Histories").HasKey(x => x.Test_History_ID);
 				builder.Property(x => x.Test_History_ID).ValueGeneratedOnAdd();
 
 				builder.HasOne(x => x.User)
 					.WithMany(x => x.Test_Histories)
-					.HasForeignKey(x => x.User_ID);
+					.HasForeignKey(x => x.User_ID)
+					.OnDelete(DeleteBehavior.Cascade);
+
 
 				builder.HasOne(x => x.Test)
 					.WithMany(x => x.Test_Histories)
-					.HasForeignKey(x => x.Test_ID);
+					.HasForeignKey(x => x.Test_ID)
+					.OnDelete(DeleteBehavior.Cascade);
 
 			});
 
 			modelBuilder.Entity<Test_Result>(builder =>
 			{
-				builder.ToTable("Test_Result").HasKey(x => x.Test_Result_ID);
+				builder.ToTable("Test_Results").HasKey(x => x.Test_Result_ID);
 				builder.Property(x => x.Test_Result_ID).ValueGeneratedOnAdd();
 
 				builder.HasOne(x => x.User)
 					.WithMany(x => x.Test_Results)
-					.HasForeignKey(x => x.User_ID);
+					.HasForeignKey(x => x.User_ID)
+					.OnDelete(DeleteBehavior.Cascade);
 
 				builder.HasOne(x => x.Test)
 					.WithMany(x => x.Test_Results)
-					.HasForeignKey(x => x.Test_ID);
+					.HasForeignKey(x => x.Test_ID)
+					.OnDelete(DeleteBehavior.Cascade);
 
 			});
 
 
 			modelBuilder.Entity<City>(builder =>
 			{
-				builder.ToTable("City").HasKey(x => x.City_ID);
+				builder.ToTable("Cities").HasKey(x => x.City_ID);
 				builder.Property(x => x.City_ID).ValueGeneratedOnAdd();
 
 			});
 
 			modelBuilder.Entity<Department>(builder =>
 			{
-				builder.ToTable("Department").HasKey(x => x.Department_ID);
+				builder.ToTable("Departments").HasKey(x => x.Department_ID);
 				builder.Property(x => x.Department_ID).ValueGeneratedOnAdd();
 
 				builder.HasOne(x => x.City)
@@ -145,22 +152,24 @@ namespace SES.Infrastructure
 
 			modelBuilder.Entity<Position>(builder =>
 			{
-				builder.ToTable("Position").HasKey(x => x.Position_ID);
+				builder.ToTable("Positions").HasKey(x => x.Position_ID);
 				builder.Property(x => x.Position_ID).ValueGeneratedOnAdd();
 
 			});
 
 			modelBuilder.Entity<Rang>(builder =>
 			{
-				builder.ToTable("Rang").HasKey(x => x.Rang_ID);
+				builder.ToTable("Rangs").HasKey(x => x.Rang_ID);
 				builder.Property(x => x.Rang_ID).ValueGeneratedOnAdd();
 
 			});
 
 			modelBuilder.Entity<User>(builder =>
 			{
-				builder.ToTable("User").HasKey(x => x.User_ID);
+				builder.ToTable("Users").HasKey(x => x.User_ID);
 				builder.Property(x => x.User_ID).ValueGeneratedOnAdd();
+				builder.Property(x => x.Login).HasMaxLength(25);
+				builder.Property(x => x.Password).HasMaxLength(10);
 
 				builder.HasOne(x => x.Department)
 					.WithMany(x => x.Users)
@@ -173,6 +182,8 @@ namespace SES.Infrastructure
 				builder.HasOne(x => x.Rang)
 					.WithMany(x => x.Users)
 					.HasForeignKey(x => x.Rang_ID);
+
+
 
 			});
 
