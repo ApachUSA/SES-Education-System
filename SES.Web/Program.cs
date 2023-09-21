837593736e0dc568bc24048feb34ede12d294188
ApachUA <4971779@gmail.com>
2023-09-21 12:41:00 +0300

    Added ConnectionStrings

--- SES.Web/Program.cs
+++ SES.Web/Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using SES.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<SES_DB_Context>(options =>
+						options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
