97d5814c02e30f0651bd41ccedbdf5ac8ed5977d
ApachUA <4971779@gmail.com>
2023-09-26 21:12:39 +0300

    Fix name

---
+++ SES.Domain/Entity/AbstractE/Abstracts.cs
@@ -0,0 +1,21 @@
+﻿using SES.Domain.Enum;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SES.Domain.Entity.AbstractE
+{
+	public class Abstracts
+	{
+		public int Abstract_ID { get; set; }
+
+		public required string Name { get; set; }
+
+		public required string URL { get; set; }
+
+		public required AreaOfStudy AreaOfStudy_ID { get; set; }
+	}
+}
