c29340e43b17b78fdfc85263dad9d98d246e64d2
ApachUA <4971779@gmail.com>
2023-09-27 19:46:52 +0300

    Developed account contoller and login page

---
+++ SES.Web/Controllers/AccountController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using SES.Domain.ViewModel;
+using SES.Service.Interfaces;
+
+namespace SES.Web.Controllers
+{
+	public class AccountController : Controller
+	{
+		private readonly IAccountService _accountService;
+
+		public AccountController(IAccountService accountService)
+		{
+			_accountService = accountService;
+		}
+
+		[HttpGet]
+		public IActionResult Login() => View();
+
+		[HttpPost]
+		public async Task<IActionResult> Login(LoginVM model)
+		{
+			if (ModelState.IsValid)
+			{
+				var response = await _accountService.Login(model);
+				if (response.StatusCode == Domain.Enum.ResponseStatus.Success)
+				{
+					await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new System.Security.Claims.ClaimsPrincipal(response.Data));
+					return RedirectToAction("Index", "Home");
+
+				}
+				ModelState.AddModelError("", response.Description);
+			}
+			return View(model);
+		}
+
+		public async Task<IActionResult> Logout()
+		{
+			await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+			return RedirectToAction("Index", "Home");
+		}
+	}
+}
