58c0147045e67af6e4cfb6bd61bcdde1f0da928a
ApachUA <4971779@gmail.com>
2023-09-28 21:43:05 +0300

    Fixed name

--- SES.Domain/ViewModel/RegisterVM.cs
+++ SES.Domain/ViewModel/RegisterVM.cs
@@ -12,22 +12,22 @@ namespace SES.Domain.ViewModel
 	{
 		public required string Snp { get; set; }
 
-		public required int Department_ID { get; set; }
+		public required int Department_ID_VM { get; set; }
 
-		public Department? Department { get; set; }
+		public Department? Department_VM { get; set; }
 
-		public required int Position_ID { get; set; }
+		public required int Position_ID_VM { get; set; }
 
-		public Position? Position { get; set; }
+		public Position? Position_VM { get; set; }
 
-		public required int Rang_ID { get; set; }
+		public required int Rang_ID_VM { get; set; }
 
-		public Rang? Rang { get; set; }
+		public Rang? Rang_VM { get; set; }
 
-		public required string Login { get; set; }
+		public required string Login_VM { get; set; }
 
-		public required string Password { get; set; }
+		public required string Password_VM { get; set; }
 
-		public required Role Role_ID { get; set; }
+		public required Role Role_ID_VM { get; set; }
 	}
 }
--- SES.Service/Mapping/MappingProfile.cs
+++ SES.Service/Mapping/MappingProfile.cs
@@ -17,7 +17,14 @@ namespace SES.Service.Mapping
 			CreateMap<RegisterVM, User>()
 			.ForMember(opt => opt.Surname, opt => opt.MapFrom(src => SplitSnp(src.Snp, 0)))
 			.ForMember(opt => opt.Name, opt => opt.MapFrom(src => SplitSnp(src.Snp, 1)))
-			.ForMember(opt => opt.Patronymic, opt => opt.MapFrom(src => SplitSnp(src.Snp, 2)));
+			.ForMember(opt => opt.Patronymic, opt => opt.MapFrom(src => SplitSnp(src.Snp, 2)))
+			.ForMember(opt => opt.Department_ID, opt => opt.MapFrom(src => src.Department_ID_VM))
+			.ForMember(opt => opt.Rang_ID, opt => opt.MapFrom(src => src.Rang_ID_VM))
+			.ForMember(opt => opt.Position_ID, opt => opt.MapFrom(src => src.Position_ID_VM))
+			.ForMember(opt => opt.Login, opt => opt.MapFrom(src => src.Login_VM))
+			.ForMember(opt => opt.Password, opt => opt.MapFrom(src => src.Password_VM))
+			.ForMember(opt => opt.PasswordConfirm, opt => opt.MapFrom(src => src.Password_VM))
+			.ForMember(opt => opt.Role_ID, opt => opt.MapFrom(src => src.Role_ID_VM));
 
 			CreateMap<Test_Result, Test_History>();
 		}
