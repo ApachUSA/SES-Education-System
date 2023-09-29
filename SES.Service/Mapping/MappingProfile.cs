fa897271bfb64ac90fbd5057905a438a22bbb83e
ApachUA <4971779@gmail.com>
2023-09-29 21:41:32 +0300

    Added reverse mapping <User, RegisterVM

--- SES.Service/Mapping/MappingProfile.cs
+++ SES.Service/Mapping/MappingProfile.cs
@@ -15,6 +15,7 @@ namespace SES.Service.Mapping
 		public MappingProfile()
 		{
 			CreateMap<RegisterVM, User>()
+			.ForMember(opt => opt.User_ID, opt => opt.MapFrom(src => src.User_ID_VM))
 			.ForMember(opt => opt.Surname, opt => opt.MapFrom(src => SplitSnp(src.Snp, 0)))
 			.ForMember(opt => opt.Name, opt => opt.MapFrom(src => SplitSnp(src.Snp, 1)))
 			.ForMember(opt => opt.Patronymic, opt => opt.MapFrom(src => SplitSnp(src.Snp, 2)))
@@ -25,6 +26,17 @@ namespace SES.Service.Mapping
 			.ForMember(opt => opt.Password, opt => opt.MapFrom(src => src.Password_VM))
 			.ForMember(opt => opt.PasswordConfirm, opt => opt.MapFrom(src => src.Password_VM))
 			.ForMember(opt => opt.Role_ID, opt => opt.MapFrom(src => src.Role_ID_VM));
+
+			CreateMap<User, RegisterVM>()
+			.ForMember(dest => dest.User_ID_VM, opt => opt.MapFrom(src => src.User_ID))
+			.ForMember(dest => dest.Snp, opt => opt.MapFrom(src => $"{src.Surname} {src.Name} {src.Patronymic}"))
+			.ForMember(dest => dest.Department_ID_VM, opt => opt.MapFrom(src => src.Department_ID))
+			.ForMember(dest => dest.Rang_ID_VM, opt => opt.MapFrom(src => src.Rang_ID))
+			.ForMember(dest => dest.Position_ID_VM, opt => opt.MapFrom(src => src.Position_ID))
+			.ForMember(dest => dest.Login_VM, opt => opt.MapFrom(src => src.Login))
+			.ForMember(dest => dest.Password_VM, opt => opt.MapFrom(src => src.Password))
+			.ForMember(dest => dest.Role_ID_VM, opt => opt.MapFrom(src => src.Role_ID));
+
 
 			CreateMap<Test_Result, Test_History>();
 		}
