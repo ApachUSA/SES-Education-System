87a0dc99ada0cf677ddafab62c79f04e6c887227
ApachUA <4971779@gmail.com>
2023-10-05 13:36:38 +0300

    Developed convert student result to pdf controller method (by iText)

--- SES.Web/Controllers/TestResultController.cs
+++ SES.Web/Controllers/TestResultController.cs
@@ -5,6 +5,15 @@ using SES.Service.Implementations;
 using SES.Service.Interfaces;
 using SES.Web.Models;
 using System.Diagnostics;
+using iText;
+using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Layout.Element;
+using iText.Layout.Properties;
+using iText.IO.Font;
+using iText.Kernel.Font;
+using iText.Layout.Borders;
+using SES.Domain.ViewModel;
 
 namespace SES.Web.Controllers
 {
@@ -58,6 +67,84 @@ namespace SES.Web.Controllers
 			return View("ErrorView", new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
 		}
 
+		[HttpGet]
+		public IActionResult StudentsResultPdf([FromQuery] string result)
+		{
+			var results = System.Text.Json.JsonSerializer.Deserialize<List<PDFResultVM>>(result);
+			using MemoryStream memoryStream = new();
+
+			var pdfDoc = new Document(new PdfDocument(new PdfWriter(memoryStream)));
+
+			string FONT_FILENAME = @"wwwroot/times.ttf";
+			PdfFont font = PdfFontFactory.CreateFont(FONT_FILENAME, PdfEncodings.IDENTITY_H);
+			pdfDoc.SetFont(font);
+
+			var title = new Paragraph("Державна служба України з надзвичайних ситуацій");
+			title.SetTextAlignment(TextAlignment.CENTER);
+			title.SetFontSize(14);
+			title.SetBold();
+			pdfDoc.Add(title);
+
+			var subtitle = new Paragraph("Результати проведення Державної кваліфікаційної атестації");
+			subtitle.SetTextAlignment(TextAlignment.CENTER);
+			subtitle.SetFontSize(12);
+			subtitle.SetBold();
+			pdfDoc.Add(subtitle);
+
+
+			var table = new Table(UnitValue.CreatePercentArray(new float[] { 1, 3, 3, 1, 2 })).UseAllAvailableWidth();
+			table.SetMarginTop(100);
+			table.AddHeaderCell(new Paragraph("№").SetTextAlignment(TextAlignment.CENTER).SetBold());
+			table.AddHeaderCell(new Paragraph("ПІБ").SetTextAlignment(TextAlignment.CENTER).SetBold());
+			table.AddHeaderCell(new Paragraph("Посада").SetTextAlignment(TextAlignment.CENTER).SetBold());
+			table.AddHeaderCell(new Paragraph("Бал").SetTextAlignment(TextAlignment.CENTER).SetBold());
+			table.AddHeaderCell(new Paragraph("Статус").SetTextAlignment(TextAlignment.CENTER).SetBold());
+
+			int i = 1;
+			foreach (var row in results)
+			{
+				table.AddCell(new Paragraph(i++.ToString()).SetTextAlignment(TextAlignment.CENTER));
+				table.AddCell(new Paragraph(row.SNP).SetTextAlignment(TextAlignment.CENTER));
+				table.AddCell(new Paragraph(row.Position).SetTextAlignment(TextAlignment.CENTER));
+				table.AddCell(new Paragraph(row.Mark).SetTextAlignment(TextAlignment.CENTER));
+				table.AddCell(new Paragraph(row.Status).SetTextAlignment(TextAlignment.CENTER));
+			}
+
+			pdfDoc.Add(table);
+
+			var footer = new Table(UnitValue.CreatePercentArray(new float[] { 2, 3, 2 })).UseAllAvailableWidth();
+			footer.SetMarginTop(100);
+			footer.SetBorder(Border.NO_BORDER);
+
+			var paragraphDate = new Paragraph();
+			var paragraphSnp = new Paragraph();
+			var paragraphSign = new Paragraph();
+
+			paragraphDate.Add(new Text("Дата: ")).Add(new Text(DateTime.Now.ToShortDateString()).SetUnderline()).Add(new Text(" р."));
+			paragraphSnp.Add(new Text(HttpContext.User.Identity.Name).SetUnderline()).Add("\n").Add(new Text("(ПІБ)").SetFontSize(8));
+			paragraphSign.Add(new Text(new string('_', 30))).Add("\n").Add(new Text("(підпис та печатка)").SetFontSize(8));
+
+
+
+			footer.AddCell(paragraphDate).SetBorder(Border.NO_BORDER);
+			footer.AddCell(paragraphSnp.SetTextAlignment(TextAlignment.CENTER));
+			footer.AddCell(paragraphSign.SetTextAlignment(TextAlignment.CENTER));
+
+			foreach (var cell in footer.GetChildren())
+			{
+				if (cell is Cell tableCell)
+				{
+					tableCell.SetBorder(Border.NO_BORDER);
+				}
+			}
+
+			pdfDoc.Add(footer);
+
+			pdfDoc.Close();
+			
+			return File(memoryStream.ToArray(), "application/pdf");
+
+		}
 
 		private static List<Test_Result> ApplyPositionFilter(List<Test_Result> results, string[]? selectedPosition) => results.Where(x => selectedPosition.Contains(x.User?.Position?.Name)).ToList();
 
