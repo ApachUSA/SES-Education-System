68d7f2f5a585686921ade73f1c1f0234e3b97b0e
ApachUA <4971779@gmail.com>
2023-10-05 13:39:27 +0300

    Developed view model for pdf result data

---
+++ SES.Domain/ViewModel/PDFResultVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SES.Domain.ViewModel
+{
+	public class PDFResultVM
+	{
+		public string? SNP { get; set; }
+		public string? Position { get; set; }
+		public string? Mark { get; set; }
+		public string? Status { get; set; }
+	}
+}
