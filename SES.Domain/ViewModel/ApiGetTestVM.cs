24106dd6b62d3fee220b66b986c789173b610ccb
ApachUA <4971779@gmail.com>
2023-10-10 20:42:54 +0300

    Created VM for Api

---
+++ SES.Domain/ViewModel/ApiGetTestVM.cs
@@ -0,0 +1,17 @@
+﻿using SES.Domain.Entity.TestE;
+using SES.Domain.Entity.UserE;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SES.Domain.ViewModel
+{
+	public class ApiGetTestVM
+	{
+		public UserApiVM User { get; set; }
+
+		public List<Question> Test { get; set; }
+	}
+}
---
+++ SES.Domain/ViewModel/UserApiVM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SES.Domain.ViewModel
+{
+	public class UserApiVM
+	{
+		public int User_ID { get; set; }
+
+		public string Snp { get; set; }
+
+		public string? Position { get; set; }
+
+		public string? Rang { get; set; }
+	}
+}
