07bc6d302b00727784abc46994c6448692897cab
ApachUA <4971779@gmail.com>
2023-10-10 20:54:04 +0300

    Realised ICommand

---
+++ SES.Desktop/ViewModel/RelayCommand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SES.Desktop.ViewModel
+{
+	public class RelayCommand : ICommand
+	{
+		private readonly Action<object> execute;
+		private readonly Predicate<object> canExecute;
+
+		public RelayCommand(Action<object> execute, Predicate<object> canExecute = null)
+		{
+			this.execute = execute ?? throw new ArgumentNullException(nameof(execute));
+			this.canExecute = canExecute;
+		}
+
+		public event EventHandler CanExecuteChanged
+		{
+			add { CommandManager.RequerySuggested += value; }
+			remove { CommandManager.RequerySuggested -= value; }
+		}
+
+		public bool CanExecute(object parameter)
+		{
+			return canExecute == null || canExecute(parameter);
+		}
+
+		public void Execute(object parameter)
+		{
+			execute(parameter);
+		}
+	}
+}
