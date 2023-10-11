62de2bcfce943ad4f41520ecd9dbc186a0f0d6b2
ApachUA <4971779@gmail.com>
2023-10-11 21:41:12 +0300

    Added a model for the result and its status

---
+++ SES.Desktop/Model/Test_Result.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SES.Desktop.Model
+{
+	public class Test_Result
+	{
+		public int Test_Result_ID { get; set; }
+
+		public required int Right_Answer_Count { get; set; }
+
+		public required int Mark { get; set; }
+
+		public required string Date { get; set; }
+
+		public required ResultStatus Status { get; set; }
+
+		public required int User_ID { get; set; }
+
+		public required int Test_ID { get; set; }
+
+	}
+}
